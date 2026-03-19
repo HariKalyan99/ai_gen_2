29af5f811e074fb03aedce1f72eac8a968eaf112
Harikalyan99 <harikalyan99tronics@gmail.com>
2026-03-19 19:55:53 +0530

    4:04, added the server, about to add clerk

---
+++ server/server.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+const app = express();
+import { clerkMiddleware } from '@clerk/express'
+// Middleware
+app.use(cors())
+app.use(express.json());
+app.use(clerkMiddleware())
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
