530b4ff6fbd4615bd623edc28e70cf577a553281
Harikalyan <harikalyan99tronics@gmail.com>
2026-03-20 17:22:11 +0530

    5:28, partial commit, accessing google cloud

---
+++ server/controllers/projectController.ts
@@ -0,0 +1,108 @@
+import { Request, Response } from "express";
+import * as Sentry from "@sentry/node";
+import { prisma } from "../configs/prisma.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const createProject = async (req: Request, res: Response) => {
+  let tempProjectId: string;
+  const { userId } = req.auth();
+  let isCreditDeducted = false;
+
+  const {
+    name = "New Project",
+    aspectRatio,
+    userPrompt,
+    productName,
+    productDescription,
+    targetLength = 5,
+  } = req.body;
+
+  const images: any = req.files;
+
+  if (images.length < 2 || !productName) {
+    return res.status(400).json({ message: "Please upload at least 2 images" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if ((!user || user.credits, 5))
+    res.status(401).json({ message: "Insufficient credits" });
+  else
+    await prisma.user
+      .update({
+        where: {
+          id: userId,
+        },
+        data: {
+          credits: { decrement: 5 },
+        },
+      })
+      .then(() => (isCreditDeducted = true));
+  // deduct credits for image generation
+
+  try {
+    let uploadedImages = await Promise.all(
+      images.map(async (img: any) => {
+        let result = await cloudinary.uploader.upload(img.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+
+    const project = await prisma.project.create({
+      data: {
+        name,
+        userId,
+        productName,
+        productDescription,
+        userPrompt,
+        aspectRatio,
+        targetLength: parseInt(targetLength),
+        uploadedImages,
+        isGenerating: true,
+      },
+    });
+
+    tempProjectId = project.id;
+  } catch (error: any) {
+    Sentry.captureException(error);
+    res.status(500).json({
+      message: error.code || error.message || "Internal server error",
+    });
+  }
+};
+
+export const createVideo = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    res.status(500).json({
+      message: error.code || error.message || "Internal server error",
+    });
+  }
+};
+
+export const getAllPublishedProjects = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    res.status(500).json({
+      message: error.code || error.message || "Internal server error",
+    });
+  }
+};
+
+export const deleteProject = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    res.status(500).json({
+      message: error.code || error.message || "Internal server error",
+    });
+  }
+};
